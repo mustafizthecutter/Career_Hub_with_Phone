ae6a1b8c10585b2ad5706c786122dcb0ed9cf289
Mustafizur Rahman <mustafizur232@gmail.com>
2023-11-29 22:55:03 +0600

    some corrections updated

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,6 @@
 import { NavLink } from "react-router-dom";
 import './Header.css'
-// import { Link } from "react-router-dom";
+
 const NavBar = () => {
     return (
         <nav>
--- src/pages/DashBoardLayOut.jsx
+++ src/pages/DashBoardLayOut.jsx
@@ -1,18 +1,19 @@
 import { Outlet } from "react-router-dom";
 import { NavLink } from "react-router-dom";
+
 const DashBoardLayOut = () => {
     return (
         <div>
             <h1 className="text-center font-bold text-3xl mb-4">this is dashboard layout</h1>
 
             <div className="flex gap-12">
-                <div>
+                <nav>
                     <div className="w-20% bg-orange-300 p-20 text-2xl  rounded-sm ">
                         <NavLink className={'block mb-4'} to={'/dashboard'}>DashBoard</NavLink>
                         <NavLink className={'block mb-4'} to={'/dashboard/profile'}>Profile</NavLink>
                         <NavLink className={'block mb-4'} to={'/dashboard/editProfile'}>EditProfile</NavLink>
                     </div>
-                </div>
+                </nav>
                 <div className="w-80%">
                     <Outlet></Outlet>
                 </div>
